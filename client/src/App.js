9d8ce4ca7ea5a0889fcbbfd16efca9f4dc82341c
kyoety <82348264+kyoety@users.noreply.github.com>
2024-08-08 10:53:54 -0400

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
+//Here are all the components we are importing
 import Main from './components/Main'
 import AddExpense from './components/Add'
 import EditExpense from './components/Edit'
@@ -10,6 +11,7 @@ export default function App() {
     <div className="container w-4/6">
       <BrowserRouter>
         <Routes>
+    //All the routes to different components/pages are here
             <Route path="/" element={<Main />}></Route>
             <Route path="/add" element={<AddExpense />}></Route>
             <Route path="/edit/:id" element={<EditExpense />}></Route>
